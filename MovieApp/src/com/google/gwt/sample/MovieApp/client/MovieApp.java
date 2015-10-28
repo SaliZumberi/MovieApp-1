8782da79ad4c2eea6bb9c2f6ab375bef8bd6f228
Mikehirt <mike.hirt@uzh.ch>
2015-10-28 19:59:11 +0100

    TestCommit

--- MovieApp/src/com/google/gwt/sample/MovieApp/client/MovieApp.java
+++ MovieApp/src/com/google/gwt/sample/MovieApp/client/MovieApp.java
@@ -4,7 +4,7 @@ package com.google.gwt.sample.MovieApp.client;
 import com.google.gwt.core.client.EntryPoint;
 
 import com.google.gwt.user.client.ui.Button;
-
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -13,8 +13,10 @@ public class MovieApp implements EntryPoint {
 
 	public void onModuleLoad() {
 		Button btn = new Button("Test");
-		// testcomment
+		Label lb = new Label("Label");
+		// test
 		RootPanel.get().add(btn);
+		RootPanel.get().add(lb);
 		
 	}
 }
